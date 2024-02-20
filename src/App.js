e352cf3fefa0a28532b15ac89c094aee88f32fac
Peace Oyewole <oyewolepeace94@gmail.com>
2024-02-20 09:30:09 -1100

    Added a footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Forecast from './Forecast.js';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Forecast />
+      <div className="container">
+        <h1 className='btn btn-primary'>Weather App</h1>
+        <Forecast />
+        <p>
+          <a href="https://github.com/Peacefsps/weather-app">Open-source code,</a> by 
+          <a href="https://github.com/Peacefsps">Peace Iwakin</a> from
+          <a href="https://www.shecodes.io/graduates/106144-peace-iwakin">She Codes</a>
+        </p>
+      </div>
     </div>
   );
 }
